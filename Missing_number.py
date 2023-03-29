e72b50ba84dea1fd914a3c0327020bcc042bcd59
Abhishek Shetty <aushetty@in.ibm.com>
2023-03-29 16:18:25 +0530

    Create Missing_number.py

---
+++ Missing_number.py
@@ -0,0 +1,25 @@
+
+#Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+nums = [3,0,1]
+nums = [0,1]
+
+
+
+
+
+class Solution:
+    def missingnumber(self , nums):
+        
+        count = len(nums)
+        for i in range(len(nums)):
+            
+            
+        return count 
+
+
+
+
+
+result = Solution().missingnumber(nums)
+print(result)
