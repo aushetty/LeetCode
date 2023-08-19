58c875883fc87d3a730b85ce8f124659802e9a06
Abhishek Shetty <shetty.abhishek42@gmail.com>
2023-08-19 16:52:07 +0530

    commit

--- BInary_search/FInd_first_last_element_in_sorted_array.py
+++ BInary_search/FInd_first_last_element_in_sorted_array.py
@@ -1,23 +1,34 @@
-nums = [5,7,7,8,8,10]
-target = 6
- 
- 
- 
+
 class Solution:
-    def searchRange():
+    def searchRange(self, nums ,target):
+
+        def binsrch(nums,target,leftb):
+            l , r = 0 , len(nums) - 1
+            i =-1
+            while l <= r:
+                mid = (l + r)//2
+
+                if  target > nums[mid]:
+                    l = mid + 1
+                elif target < nums[mid]:
+                    r = mid -1
+                else:
+                    i = mid
+                    if leftb:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+            return i
+
         
-        
-        
-        
-        
-        return None
+        left = binsrch(nums, target, True)
+        print(left)
+        right = binsrch(nums, target, False)
+        print(right)
 
+        return [left, right]
 
-
-
-
-
-result = Solution().searchRange()
+result = Solution().searchRange(nums = [5,7,7,8,8,10], target = 8)
 print(result)
  
  
