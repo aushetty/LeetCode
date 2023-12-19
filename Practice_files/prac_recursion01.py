d4d83e313b51150462d5a93f314f836ee7de5756
Abhishek Shetty <shetty.abhishek42@gmail.com>
2023-12-19 17:33:59 +0530

    new commit

--- Practice_files/prac_recursion01.py
+++ Practice_files/prac_recursion01.py
@@ -1,22 +1,36 @@
 #recursive approach
-# def find_sum(n):
-
-#     if n == 1:
-#         return n
-    
-#     return n + find_sum(n-1)
-
-#iterative approach
 def find_sum(n):
 
-    sum = 0
+    if n == 1:
+        return n
+    
+    return n + find_sum(n-1)
 
-    for i in range(1,n+1):
-        sum += i
-    return sum 
+#iterative approach
+# def find_sum(n):
+
+#     sum = 0
+
+#     for i in range(1,n+1):
+#         sum += i
+#     return sum 
+
+
+
+#fibonacci series
+
+def fib(n):
+
+
+    if n == 0 or n == 1:
+        return n 
+    
+    
+    return fib(n-1) + fib(n-2)
 
 
 
 
 if __name__ == '__main__':
     print(find_sum(7))
+    print(fib(6))
