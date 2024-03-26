832a4c938f1eb3ff287cd77fba3deebc5226365e
Abhishek Shetty <shetty.abhishek42@gmail.com>
2024-03-26 13:20:08 +0530

    commit

---
+++ Treeeeee/Level_order_traversal/balanced_binary_tree110.py
@@ -0,0 +1,33 @@
+# <!-- https://leetcode.com/problems/balanced-binary-tree/description/
+
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: true
+
+
+# Given a binary tree, determine if it is
+# height-balanced.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def helper(root):
+            if not root: return [True, 0]
+
+            left , right = helper(root.left) , helper(root.right)
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+
+            return [balanced , 1 + max(left[1] , right[1])]
+
+        return helper(root)[0]
+        
+
+
+
