767bc46e1b3f035f8636d41b77c9cddcf7997044
Abhishek Shetty <shetty.abhishek42@gmail.com>
2024-05-28 15:35:53 +0530

    game.py

---
+++ SystemDesign/LLD/LLD-Game-engine/game.py
@@ -0,0 +1,42 @@
+import os 
+import sys
+
+def start():
+    return None
+
+
+def move(board, player, move):
+    return None
+
+def GameResultisComplete(board):
+
+
+
+     
+    return None
+
+
+
+
+
+
+
+
+
+def main():
+    
+
+
+
+    return None 
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
